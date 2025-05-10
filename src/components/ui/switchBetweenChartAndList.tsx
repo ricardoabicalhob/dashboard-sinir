353e435b8eb39f53fe4a5182ef267e37f5dd98c9
ricardoabicalhob <rbicalho2014@gmail.com>
2025-05-10 01:01:22 -0300

    modificar estilos dos botões do componente switch

--- src/components/ui/switchBetweenChartAndList.tsx
+++ src/components/ui/switchBetweenChartAndList.tsx
@@ -1,4 +1,5 @@
 import { ChartColumnBig, List } from "lucide-react"
+import { useState } from "react"
 
 interface SwitchBetweenChartAndListProps {
     handleShowChartManifests :()=> void
@@ -6,21 +7,65 @@ interface SwitchBetweenChartAndListProps {
 }
 
 export default function SwitchBetweenChartAndList({ handleShowChartManifests, handleShowListManifests } :SwitchBetweenChartAndListProps) {
+    
+    const [ disableChartButton, setDisableChartButton ] = useState(true)
+    const [ disableListButton, setDisableListButton ] = useState(false)
+
+    function handleDisableChartButton() {
+        setDisableChartButton(true)
+        setDisableListButton(false)
+    }
+
+    function handleDisableListButton() {
+        setDisableListButton(true)
+        setDisableChartButton(false)
+    }
+
     return(
         <div className="flex gap-2 -mt-4">
-                <div 
-                    className="flex w-fit h-fit p-2 bg-[#00BCD4] shadow-md shadow-gray-500 rounded-xl cursor-pointer"
-                    onClick={()=> handleShowChartManifests()}    
-                >
-                    <ChartColumnBig className="w-4 h-4 text-white"/>
-                </div>
+                {
+                    disableChartButton &&
+                        <div 
+                            className="flex w-fit h-fit p-2 bg-gray-300 shadow-md shadow-gray-500 rounded-xl"    
+                        >
+                            <ChartColumnBig className="w-4 h-4 text-black/20"/>
+                        </div>
+                }
 
-                <div 
-                    className="flex w-fit h-fit p-2 bg-[#00BCD4] shadow-md shadow-gray-500 rounded-xl cursor-pointer"
-                    onClick={()=> handleShowListManifests()}
-                >
-                    <List className="w-4 h-4 text-white"/>
-                </div>
+                {
+                    !disableChartButton &&
+                        <div 
+                            className="flex w-fit h-fit p-2 bg-[#00BCD4] shadow-md shadow-gray-500 rounded-xl cursor-pointer"
+                            onClick={()=> {
+                                handleShowChartManifests()
+                                handleDisableChartButton()
+                            }}    
+                        >
+                            <ChartColumnBig className="w-4 h-4 text-white"/>
+                        </div>
+                }
+
+                {
+                    disableListButton &&
+                        <div 
+                            className="flex w-fit h-fit p-2 bg-gray-300 shadow-md shadow-gray-500 rounded-xl"    
+                        >
+                            <List className="w-4 h-4 text-black/20"/>
+                        </div>
+                }
+
+                {
+                    !disableListButton && 
+                        <div 
+                            className="flex w-fit h-fit p-2 bg-[#00BCD4] shadow-md shadow-gray-500 rounded-xl cursor-pointer"
+                            onClick={()=> {
+                                handleShowListManifests()
+                                handleDisableListButton()
+                            }}
+                        >
+                            <List className="w-4 h-4 text-white"/>
+                        </div>
+                }
             </div>
     )
 }
